bee10ace17da90e0e603be53c02d77a5058a8edb
[pawel] <[szpytmapawel@hotmail.com]>
2020-02-05 12:43:27 +0000

    commit

---
+++ 30DaysOfCode/Day16-Exceptions/src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String S = in.next();
+    try{
+      //trying to parse given String to Integer
+      System.out.println(Integer.parseInt(S));
+    }catch(Exception e){
+
+      // If our string is not a number print Exception message
+      System.out.println("Bad String");
+    }
+  }
+}
+
+
